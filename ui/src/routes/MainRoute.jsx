2bfa5ea96b5c0fa62f78b7e34a5f1fb43343a228
Sethumadhavan K <sethuksethu@gmail.com>
2024-09-23 17:45:09 +0530

    redirect to preview page

--- ui/src/routes/MainRoute.jsx
+++ ui/src/routes/MainRoute.jsx
@@ -1,14 +1,16 @@
 
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Navigate} from "react-router-dom"
 import DashboardLayout from "src/layouts/dashboard/Dashboard"
 import routes from "src/config/routes"
 import Chat from "src/pages/Chat/Chat"
+import { v4 } from "uuid"
 
 const MainRoute = ()=>{
 
     return(
         <Routes>
             <Route key={"index"} path="/:contextId/chat" element={<Chat/>} />
+            <Route path="/"  element={ <Navigate to={`/preview/${v4()}/chat`} repalce={true} /> } />
             <Route path='/' element={<DashboardLayout/>}>
                 {
                 routes.map((item, index)=>{
