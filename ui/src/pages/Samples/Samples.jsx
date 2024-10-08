2fe4ff4fcac158e93397468db6f737af33c0f778
zteezy11 <recoverywaiyan19@gmail.com>
2024-10-08 13:50:27 +0800

    Add delete functionality for samples

--- ui/src/pages/Samples/Samples.jsx
+++ ui/src/pages/Samples/Samples.jsx
@@ -3,48 +3,67 @@ import EmptySample from "./EmptySample"
 import Modal from "src/components/Modal/Modal"
 import { useEffect, useState } from "react"
 import SampleForm from "./SampleForm"
-import { getSamples } from "src/services/Sample"
+import { getSamples, deleteSample } from "src/services/Sample"
 import SampleList from "./SampleList"
+import { confirmDialog } from "src/utils/confirmDialog"
 
+const Samples = () => {
+  const [sampleList, setSampleList] = useState([])
+  const [showSampleModal, setSampleModal] = useState(false)
+  const [editSample, setEditSample] = useState({})
 
-const Samples = ()=>{
+  const getAllSamples = () => {
+    getSamples().then(response => {
+      setSampleList(response.data?.data?.sql ?? [])
+    })
+  }
 
-    const [sampleList, setSampleList] = useState([])
-    const [showSampleModal, setSampleModal] = useState(false)
-    const [editSample, setEditSample] = useState({})
+  const onEdit = (sampleData) => {
+    setSampleModal(true)
+    setEditSample(sampleData)
+  }
 
-    const  getAllSamples = ()=>{
-        getSamples().then(response=>{
-            setSampleList(response.data?.data?.sql ?? [])
+  const onCreateNew = () => {
+    setSampleModal(true)
+    setEditSample({})
+  }
+
+  const onDelete = (sampleData) => {
+    confirmDialog({
+      title: "Delete Sample",
+      message: "Are you sure you want to delete this sample?",
+      onConfirm: () => {
+        deleteSample(sampleData.id).then(() => {
+          getAllSamples()
+        }).catch(error => {
+          console.error("Error deleting sample:", error)
+          // You can add a simple alert here if you want to show an error message
+          alert("Failed to delete sample. Please try again.")
         })
-    }
+      }
+    })
+  }
 
-    const onEdit = (sampleData)=>{
-        setSampleModal(true)
-        setEditSample(sampleData)
-    }
+  useEffect(() => {
+    getAllSamples()
+  }, [])
 
-    const onCreateNew = ()=>{
-        setSampleModal(true)
-        setEditSample({})
-    }
-
-
-    useEffect(()=>{
-        getAllSamples()
-    }, [])
-
-    return(
-       <DashboardBody title="Samples">
-
-        { sampleList?.length == 0 && <EmptySample onCreateClick={()=>setSampleModal(true)} /> }
-        { sampleList?.length > 0 && <SampleList data={sampleList} onCreate={onCreateNew} onEdit={onEdit} /> }
-
-        <Modal title="Create Sample" show={showSampleModal} onClose={()=>setSampleModal(false)} >
-            <SampleForm sample={editSample} afterCreate={getAllSamples} onCancel={()=>setSampleModal(false)} />
-        </Modal>
-       </DashboardBody>
-    )
+  return (
+    <DashboardBody title="Samples">
+      {sampleList?.length == 0 && <EmptySample onCreateClick={() => setSampleModal(true)} />}
+      {sampleList?.length > 0 && (
+        <SampleList
+          data={sampleList}
+          onCreate={onCreateNew}
+          onEdit={onEdit}
+          onDelete={onDelete}
+        />
+      )}
+      <Modal title="Create Sample" show={showSampleModal} onClose={() => setSampleModal(false)}>
+        <SampleForm sample={editSample} afterCreate={getAllSamples} onCancel={() => setSampleModal(false)} />
+      </Modal>
+    </DashboardBody>
+  )
 }
 
 export default Samples
