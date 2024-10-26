81cf47f41daf5a9360cb3f5868b580551ec15b9a
nishammahsin <nisham.mahsin@gmail.com>
2024-10-26 17:04:38 +0530

    refactor: export customXAxisLabel and customYAxisLabel components
    - Moved customXAxisLabel and customYAxisLabel components outside of LineChart component
    - Exported customXAxisLabel and customYAxisLabel for reuse in test
    
    Improves code organization and reusability of custom axis label components.

--- ui/src/components/Chart/LineChart/LineChart.jsx
+++ ui/src/components/Chart/LineChart/LineChart.jsx
@@ -1,28 +1,26 @@
-
 import { LineChart as ReLineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 import style from "../style.module.css"
 
 
-const LineChart = ({title ="Line Chart", data = [], xAxis = "name", yAxis = "value", dataLength = 12})=>{
-
-    const customXAxisLabel = ({ payload, x, y, width, height })=>{
+const customXAxisLabel = ({ payload, x, y, width, height })=>{
        
-        return(
-            <text orientation="bottom" width={width} height={height} stroke="none" x={x} y={y + 10}  style={{direction:"rtl",fontSize:"11px",fontFamily:"Inter",color:"rgba(28, 28, 28, 0.40",textAlign:"center",fontStyle:"normal", fontWeight:"300", lineHeight:"18px",opacity:"60%"}} fill="#888787" >
-                <tspan x={x+20} y={y+10}>{payload.value}</tspan>
-            </text>
-        )
-    }
+    return(
+        <text orientation="bottom" width={width} height={height} stroke="none" x={x} y={y + 10}  style={{direction:"rtl",fontSize:"11px",fontFamily:"Inter",color:"rgba(28, 28, 28, 0.40",textAlign:"center",fontStyle:"normal", fontWeight:"300", lineHeight:"18px",opacity:"60%"}} fill="#888787" >
+            <tspan x={x+20} y={y+10}>{payload.value}</tspan>
+        </text>
+    )
+}
 
-    const customYAxisLabel = ({ payload, x, y, width, height })=>{
-        
-        return(
-            <text orientation="bottom" width={width} height={height} stroke="none" x={x} y={y + 10}  style={{direction:"rtl", fontSize:"11px",fontFamily:"Inter",color:"rgba(28, 28, 28, 0.40",textAlign:"center",fontStyle:"normal",fontWeight:"300", lineHeight:"18px", opacity:"60%"}} fill="#888787">
-                <tspan x={x-5} y={y}>{payload.value}</tspan>
-            </text>
-        )
-    }
+const customYAxisLabel = ({ payload, x, y, width, height })=>{
+    
+    return(
+        <text orientation="bottom" width={width} height={height} stroke="none" x={x} y={y + 10}  style={{direction:"rtl", fontSize:"11px",fontFamily:"Inter",color:"rgba(28, 28, 28, 0.40",textAlign:"center",fontStyle:"normal",fontWeight:"300", lineHeight:"18px", opacity:"60%"}} fill="#888787">
+            <tspan x={x-5} y={y}>{payload.value}</tspan>
+        </text>
+    )
+}
 
+const LineChart = ({title ="Line Chart", data = [], xAxis = "name", yAxis = "value", dataLength = 12})=>{
 
     return(
         <>
@@ -48,3 +46,4 @@ const LineChart = ({title ="Line Chart", data = [], xAxis = "name", yAxis = "val
 
 
 export default LineChart
+export { customXAxisLabel, customYAxisLabel };
