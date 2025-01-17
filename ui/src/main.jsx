a8f716fee9a73be348fbbbaf0e253795d4baf136
amilmshaji <msamilmanadiyil2560@gmail.com>
2025-01-17 18:19:04 +0530

    Revert "Merge branch 'sirocco-ventures:main' into main"
    This reverts commit 3d921ce2a69017b9feda21faa92e93761c4ade9f, reversing
    changes made to 7026be0b4c155b45f9beabc274cee94f903d9ba2.

--- ui/src/main.jsx
+++ ui/src/main.jsx
@@ -7,7 +7,7 @@ import "./global.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <BrowserRouter basename="/ui">
+    <BrowserRouter>
         <App />
         <ToastContainer/>
     </BrowserRouter>
